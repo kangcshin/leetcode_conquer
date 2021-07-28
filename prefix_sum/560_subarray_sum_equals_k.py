55da6714f90bafaa269327bdcb4aaef1bf7f89d7
Kang Shin <11722907+kangcshin@users.noreply.github.com>
2021-07-28 14:33:35 -0400

    Update 560_subarray_sum_equals_k.py

--- prefix_sum/560_subarray_sum_equals_k.py
+++ prefix_sum/560_subarray_sum_equals_k.py
@@ -15,3 +15,32 @@ Output: 2
 
 '''
 
+# Brute Force
+class Solution:
+  def subarraaySum(self, nums:List[int], k:int) -> int:
+    result = 0
+    for i in range(len(nums)):
+      temp_sum = nums[i]
+      if temp_sum == k:
+        result += 1
+      for j in range(i+1, len(nums)):
+        temp_sum += nums[j]
+        if temp_sum == k:
+          result += 1
+    return result
+  
+# Prefix_Sum
+class Solution:
+  def subarraaySum(self, nums:List[int], k:int) -> int:
+    result = 0
+    prefix_sum = collections.defaultdict(int)
+    # key: prefix_sum, val: frequency
+    prefix_sum[0] = 1
+    cur_sum = 0
+    for idx, val in enumerate(nums):
+      cur_sum += val
+      if cur_sum - k in prefix_sum:
+        result += prefix_sum[cur_sum - k]
+      prefix_sum[cur_sum] += 1
+    return result
+  
