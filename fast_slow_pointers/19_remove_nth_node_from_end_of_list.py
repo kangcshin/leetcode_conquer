21da6d501fa012a5ee7296dffbddea221c70331f
Kang Shin <kangcshin@gmail.com>
2021-08-10 20:40:36 -0400

    .

--- fast_slow_pointers/19_remove_nth_node_from_end_of_list.py
+++ fast_slow_pointers/19_remove_nth_node_from_end_of_list.py
@@ -39,6 +39,11 @@ F,S
          [1] -> [2] -> [4] -> [5]
 '''
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
