2e60a065a544b639be85887ef0024757a1ffdec2
kangcshin <kangcshin@gmail.com>
2022-10-30 01:24:16 +0000

    Oct 29, 2022, 6:24 PM

---
+++ linked_list/palindrome_linked_list.py
@@ -0,0 +1,7 @@
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+# Input: head = [1,2,2,1]
+# Output: true
+
+# Input: head = [1,2]
+# Output: false
