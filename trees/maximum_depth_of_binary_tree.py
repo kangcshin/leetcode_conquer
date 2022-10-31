1d348e980eedea3b22a0c07486660234a3e29f4f
kangcshin <kangcshin@gmail.com>
2022-10-31 01:23:23 +0000

    Oct 30, 2022, 6:23 PM

--- trees/maximum_depth_of_binary_tree.py
+++ trees/maximum_depth_of_binary_tree.py
@@ -8,3 +8,21 @@
 # Input: root = [1,null,2]
 # Output: 2
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # use DFS (depth first search) and recursion
+        # base case
+        if root is None:
+            return 0
+        
+        # recursion 
+        left = self.maxDepth(root.left) + 1
+        right = self.maxDepth(root.right) + 1
+        
+        return max(left, right)
