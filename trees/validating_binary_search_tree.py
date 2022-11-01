bf54a0521695f09f4f21d4cfd8ee911d92f2947b
kangcshin <kangcshin@gmail.com>
2022-11-01 20:05:37 +0000

    Nov 1, 2022, 1:05 PM

--- trees/validating_binary_search_tree.py
+++ trees/validating_binary_search_tree.py
@@ -13,3 +13,24 @@
 # Input: root = [5,1,4,null,null,3,6]
 # Output: false
 # Explanation: The root node's value is 5 but its right child's value is 4.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # recursion with in-order traversal (Left->Node->Right)
+        def inorder(root):
+            if not root:
+                return True
+            if not inorder(root.left):
+                return False
+            if root.val <= self.prev:
+                return False
+            self.prev = root.val
+            return inorder(root.right)
+        self.prev = -math.inf
+        return inorder(root)
