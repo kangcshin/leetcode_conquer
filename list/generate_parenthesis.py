1a52a1f25b9f2a979c2044753c77b15d68776b0d
kangcshin <kangcshin@gmail.com>
2022-11-02 05:11:16 +0000

    Nov 1, 2022, 10:11 PM

--- list/generate_parenthesis.py
+++ list/generate_parenthesis.py
@@ -12,3 +12,34 @@
 # Output: ["()"]
 
 
+def generateParenthesis(self, n: int) -> List[str]:
+    # The idea is to add ')' only after valid '('
+    # We use two integer variables left & right to see how many '(' & ')' are in the current string
+    # If left < n then we can add '(' to the current string
+    # If right < left then we can add ')' to the current string
+	def dfs(left, right, s):
+		if len(s) == n * 2:
+			res.append(s)
+			return 
+
+		if left < n:
+			dfs(left + 1, right, s + '(')
+
+		if right < left:
+			dfs(left, right + 1, s + ')')
+
+	res = []
+	dfs(0, 0, '')
+	return res
+
+    # 			   	(0, 0, '')
+    # 			 	    |	
+    # 				(1, 0, '(')  
+    # 			   /           \
+    # 		(2, 0, '((')      (1, 1, '()')
+    # 		   /                 \
+    # 	(2, 1, '(()')           (2, 1, '()(')
+    # 	   /                       \
+    # (2, 2, '(())')                (2, 2, '()()')
+    # 	      |	                             |
+    # res.append('(())')             res.append('()()')
