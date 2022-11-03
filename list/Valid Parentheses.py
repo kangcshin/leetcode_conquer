6d229c6b94aacc37355a5bc7939d8e30d8b40060
kangcshin <kangcshin@gmail.com>
2022-11-03 04:43:44 +0000

    Nov 2, 2022, 9:43 PM

--- list/Valid Parentheses.py
+++ list/Valid Parentheses.py
@@ -20,3 +20,21 @@
 # Input: s = "(]"
 # Output: false
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_mapping = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        for bracket in s:
+            if bracket not in bracket_mapping:
+                stack.append(bracket)
+            elif stack and stack[-1] == bracket_mapping[bracket]:
+                stack.pop()
+            else:
+                return False
+        
+        return len(stack) == 0
